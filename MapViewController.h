e84bc7e12cf133c28bea40185af941100ec2135b
Christian Navarrete <idinkyy@gmail.com>
2014-10-29 21:33:54 -0700

    Set up storyboard

--- MapViewController.h
+++ MapViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  MapViewController.h
 //  Shred
 //
-//  Created by HoodsDream on 2/27/14.
+//  Created by HoodsDream on 7/3/14.
 //  Copyright (c) 2014 HoodsDream. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MapViewController : UIView
 
 @end
---
+++ MapViewController.m
@@ -0,0 +1,31 @@
+//
+//  MapViewController.m
+//  Shred
+//
+//  Created by HoodsDream on 7/3/14.
+//  Copyright (c) 2014 HoodsDream. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@implementation MapViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
